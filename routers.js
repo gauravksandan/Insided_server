4c99acd45b3323f2840398d63f0a4b3dfc875ff8
gauravksandan <112536600+gauravksandan@users.noreply.github.com>
2024-08-09 15:22:34 +0530

    Update routers.js

--- routers.js
+++ routers.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = (module.exports = express.Router());
-const { getUsers, getLeaderboardsByPoints } = require("./getData");
+const { getUsers, getLeaderboardsByPoints,getLeaderboardsperiod } = require("./getData");
 router.get("/api/users", getUsers);
 router.get("/api/points", getLeaderboardsByPoints);
+router.get("/api/period/weekly", getLeaderboardsperiod);
+
+
+
+
+
