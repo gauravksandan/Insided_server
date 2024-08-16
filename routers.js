64eb20cc04eab091fe47ea1b356b1702a23add63
gauravksandan <gaurav.sandan@grazitti.com>
2024-08-16 12:09:47 +0530

    routers

--- routers.js
+++ routers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = (module.exports = express.Router());
-const { getUsers, getLeaderboardsByPoints,getLeaderboardsperiod,getLogedInUser } = require("./getData");
+const { getUsers, getLeaderboardsByPoints,getLeaderboardsperiod,getLogedInUser,getArticles } = require("./getData");
 router.get("/api/users", getUsers);
 router.get("/api/points", getLeaderboardsByPoints);
 router.get("/api/articles", getArticles);
