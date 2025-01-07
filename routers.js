e30a121366d7df1620b47c040e73a5ec44c64371
gauravksandan <112536600+gauravksandan@users.noreply.github.com>
2025-01-07 12:40:50 +0530

    Update routers.js
    This is for the badges data

--- routers.js
+++ routers.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = (module.exports = express.Router());
-const { getUsers, getLeaderboardsByPoints,getLeaderboardsperiod,getLogedInUser,getArticles,getCategoriesList, getArticlesv2 } = require("./getData");
+const { getUsers, getLeaderboardsByPoints,getLeaderboardsperiod,getLogedInUser,getArticles,getCategoriesList, getArticlesv2, giveBadge } = require("./getData");
 router.get("/api/users", getUsers);
 router.get("/api/points", getLeaderboardsByPoints);
 router.get("/api/articles", getArticles);
 router.get("/api/users/:id", getLogedInUser);
 router.get("/api/categories", getCategoriesList);
 router.get("/api/articlesv2", getArticlesv2);
+router.get("/api/badges", giveBadge);
 
